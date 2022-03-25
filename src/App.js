02ff5a9c6ab4e128595912e1c2e76947407dff59
foadtkf <fakhrulif@gmail.com>
2022-03-26 03:11:57 +0600

    installed bootstrap

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+function App() {
+  return (
+    <div className="App">
+      <button className='btn btn-success'>hello</button>
+    </div>
+  );
+}
+
+export default App;
