2e61b99eb0d433a838957d9ca564c2395988fb28
foadtkf <fakhrulif@gmail.com>
2022-03-26 03:42:07 +0600

    navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Header from './Components/Header/Header';
 function App() {
   return (
     <div className="App">
-      <button className='btn btn-success'>hello</button>
+      <Header></Header>
     </div>
   );
 }
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+const Header = () => {
+    return (
+        <nav class="navbar navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href=".">My Best Laptop 💻</a>
+  </div>
+</nav>
+    );
+};
+
+export default Header;
